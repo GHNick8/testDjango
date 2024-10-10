f1443de3a3b4163245bae3e208630f2e26c9363b
DjangoNick <nickjanssens2030@gmail.com>
2024-10-10 18:32:23 +0200

    toevoegen untracked files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,14 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-# Security settings
-from decouple import config 
-SECRET_KEY = config('SECRET_KEY') 
-DEBUG = config('DEBUG', default=False, cast=bool)
+# 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY='django-insecure-+pd+0=kj$eq-%lk#spyljbu6)0y2$tg#^2skfnb)6w@1m_vc%5'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
